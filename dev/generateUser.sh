3b7bbd15e78d516ca99623bf5ac7e72c839f342c
David Lars Ketch <davidlarsketch@gmail.com>
2018-05-12 17:32:10 +0000

    Adds flags for generateUser enabling calling it with specific # of reps

--- dev/generateUser.sh
+++ dev/generateUser.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+while getopts r: opt; do
+	case $opt in
+		r) reps=$([[ "$OPTARG" -gt 0 ]] && echo "$OPTARG" || echo 10 )
 
-file="data/user.json"
+	esac
+done
+
 echo -e "generateUser started\n"
+
+file="$(dirname $0)/data/user.json"
 if [ ! -r "$file" ];
 then
 	touch $file
 fi
 
-#Generaes 10 users and their devices
-for ((i=0;i<10;i++));
+#Generaets $reps users or 10 users and their devices
+for ((i=0;i<$reps;i++));
 do
 	uid=u$RANDOM$RANDOM
 	json="{
@@ -17,12 +24,13 @@ do
 		}
 	}"
 	
-	curl -fs -X POST -H "Content-Type: application/json" -d "$json" https://bpo0hlxopi.execute-api.us-east-1.amazonaws.com/dev//user
+	curl -fs -X POST -H "Content-Type: application/json" -d "$json" https://bpo0hlxopi.execute-api.us-east-1.amazonaws.com/dev//user > /dev/null
 	
 	if [ $? -ne 22 ];
 	then
+		echo -e "Created user $uid\n"
 		echo -e $json >> $file 
-		/bin/bash ./generateDevice.sh "$uid"
+		/bin/bash ./generateDevice.sh -u "$uid" -r 0
 	else
 		echo "Failed curl request"
 		exit
