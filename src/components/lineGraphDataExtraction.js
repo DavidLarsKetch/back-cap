3fd57c0c09b662bb1da6b078ee0f00a2bcde224d
David Lars Ketch <davidlarsketch@gmail.com>
2018-05-15 20:32:25 -0500

    Cleans up display of line graph

--- src/components/lineGraphDataExtraction.js
+++ src/components/lineGraphDataExtraction.js
@@ -1,16 +1,19 @@
 'use strict'
+import moment from 'moment'
 
 export function formatLineGraphData (obj) {
-  const timestampsArray = obj.map(i => i.Timestamp)
+  const timestampsArray = obj.map(i => moment(`${i.Timestamp}-05:00`).format('Do, h:mm:ss a'))
   const readingsArray = obj.map(i => +i.Reading)
   return {
     labels: [...timestampsArray],
     datasets: [
       {
-        label: 'Readings',
+        label: 'Reading',
         borderColor: 'red',
-        backgroundColor: 'transparent',
-        data: [...readingsArray]
+        borderWidth: '2',
+        pointRadius: '0',
+        fill: 'none',
+        data: [...readingsArray, ]
       }
     ]
   }
