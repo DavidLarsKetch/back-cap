2835ad4f2a5cfe4c644f9c8a562a4b650843af5d
David Lars Ketch <davidlarsketch@gmail.com>
2018-05-17 09:56:04 -0500

    userPicker: code clean up

--- src/components/userPicker.vue
+++ src/components/userPicker.vue
@@ -14,32 +14,31 @@ axios.defaults.headers.common['Access-Control-Allow-Origin']
 
 export default {
   name: "userPicker",
-  data () {
+  data() {
     return {
       title: 'Pick a user:',
-      errorMsg: '',
       url: 'https://bpo0hlxopi.execute-api.us-east-1.amazonaws.com/dev//user',
       userId: null,
       users: null
     }
   },
-  created () {
+  created() {
     this.getAllUsers()
   },
   methods: {
-    userSelected () {
+    userSelected() {
       this.$emit('userselected', this.userId)
     },
-    getAllUsers () {
+    getAllUsers() {
       const vm = this
 
       axios.get(vm.url)
       .then(({ status, data: { body: { Items } } }) => {
-        if (status !== 200) return vm.errorMsg = 'Users not found!'
+        if (status !== 200) return vm.$emit('error', 'Users not found!')
         vm.users = Items
       })
       .catch(err => {
-        vm.errorMsg = `This is your User error message: ${err}`
+        vm.$emit('error', `User - ${err}`)
       })
     }
   }
